3813b30d97012f5996bbf69191c518d48b1c3ae5
Ian Speelman <ianspeelman@gmail.com>
2024-11-08 10:12:19 +0100

    add types for the information coming in via the request body

--- src/Utils/types.ts
+++ src/Utils/types.ts
@@ -6,5 +6,30 @@ export type orderType = {
 export type orderProduct = {
     id: number,
     order: number,
-    product: number
+    product: number,
+    amount: number
+}
+
+export type orderRequest = {
+    user: number,
+    products: orderProductRequest[]
+}
+
+export type orderProductRequest = {
+    amount: number,
+    product: product
+}
+
+export type product = {
+    id: number,
+    name: string,
+    image: string,
+    price: number,
+    category: string,
+    specifications: string,
+    description: string,
+    stock?: number,
+    amount?: number,
+    createdAt?: Date
+    updatedAt?: Date
 }
