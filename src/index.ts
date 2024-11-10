1bf2c68caa20c1e84966f18702542093ee156b2f
Ian Speelman <ianspeelman@gmail.com>
2024-11-10 14:17:23 +0100

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import orderRoutes from "./Routes/orderRoutes"
 import bodyParser from "body-parser";
+import cors from 'cors'
 
 const app = express()
 
-
+app.use(cors())
 app.use(bodyParser.json())
 app.use('/orders', orderRoutes)
 
